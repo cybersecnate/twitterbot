572ae6f5eb3bb5c00ecf0ae8320601c94025e6bf
NathanSilvaSantos <nathan013silva@gmail.com>
2020-12-08 14:56:54 -0300

    Realizando Login

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 require('dotenv').config();
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch({
+      headless: false,
+    });
+    const page = await browser.newPage();
+    // Entra na página de login do twitter
+    await page.goto('https://twitter.com/login');
+
+    // Acessa a conta
+    await page.type('[name="session[username_or_email]"]', process.env.TWITTER_USERNAME, { delay: 20 });
+    await page.type('[name="session[password]"]', process.env.TWITTER_PASSWORD, { delay: 20 });
+    
+    await page.click('div[data-testid="LoginForm_Login_Button"]');
+  
+    await page.waitForNavigation();
+  
+    //await browser.close();
+})();
